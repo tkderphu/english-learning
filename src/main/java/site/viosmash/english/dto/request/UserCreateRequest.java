9e315507bc6c49ce740101b4d0e29577bcbe985a
tkderphu <quangphu.re050204@gmail.com>
2026-03-26 20:00:02 +0700

    fix swagger and query

--- src/main/java/site/viosmash/english/dto/request/UserCreateRequest.java
+++ src/main/java/site/viosmash/english/dto/request/UserCreateRequest.java
@@ -1,5 +1,6 @@
 package site.viosmash.english.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,13 +10,16 @@ import lombok.Data;
 public class UserCreateRequest {
 
     @NotBlank(message = "Email is required")
+    @Schema(defaultValue = "admin@gmail.com")
     @Email(message = "Email must be a valid email address")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Schema(defaultValue = "admin2004")
     @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
+    @Schema(defaultValue = "admin 20024")
     @NotBlank(message = "Full name is required")
     private String fullName;
 }
